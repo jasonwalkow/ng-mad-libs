3d0e23cfb16ebb0e2d6919e5be52f37c2b7a172b
Jason Walkow <jason.walkow1@gmail.com>
2015-09-27 21:33:53 -0700

    added user experience

--- js/app.js
+++ js/app.js
@@ -1,28 +1,42 @@
 angular.module('madLibsApp', [])
 	.controller('MyCtrl', ['$scope', function($scope) {
-	    $scope.job_title = 'Job Title';
-	    $scope.tedious_task = 'Tedious Task';
-	    $scope.dirty_task = 'Dirty Task';
-	    $scope.celebrity =  'Celebrity';
-	    $scope.useless_skill = 'Useless Skill';
-	    $scope.obnoxious_celebrity = 'Obnoxious Celebrity';
-	    $scope.huge_number = 999999;
-	    $scope.adjective =  'Adjective';
-	    $scope.Name = 'Name';
+	
 	    $scope.gender_title = 'Gender';
+
+	    $scope.hide = false;
+
+	    $scope.info = {
+		    genderSingular: "he",
+		    genderObjective: "him",
+		    genderPossessive: "his"
+		};
+
+
+    	$scope.checkValid = function() {
+		    if ($scope.madForm.$submitted && $scope.madForm.$valid && !($scope.madForm.$pristine)) {
+		      $scope.hide = true;
+		      return true;
+		    }
+		  };
+
+    	$scope.reset = function(){
+        	$scope.hide = false;
+        	$scope.submitted = false;
+        	$scope.info = {};
+        	$scope.madForm.$setPristine();
+    	}
+
 	    $scope.gender = function(){
-	      if($scope.genderSingular === 'she') {
-	      	$scope.gender_title = 'Male';
-	        $scope.Name = 'Male Name';
-	        $scope.genderSingular = 'he';
-	        $scope.genderObjective = 'him';
-	        $scope.genderPossessive = 'his';
+	      if($scope.info.genderSingular === 'she') {
+	      	$scope.info.gender_title = 'Male';
+	        $scope.info.genderSingular = 'he';
+	        $scope.info.genderObjective = 'him';
+	        $scope.info.genderPossessive = 'his';
 	      } else {
-	      	$scope.gender_title = 'Female';
-	        $scope.Name = 'Female Name';
-	        $scope.genderSingular = 'she';
-	        $scope.genderObjective = 'her';
-	        $scope.genderPossessive = 'her';
+	      	$scope.info.gender_title = 'Female';
+	        $scope.info.genderSingular = 'she';
+	        $scope.info.genderObjective = 'her';
+	        $scope.info.genderPossessive = 'her';
 	      }
 	      };
 }]);
